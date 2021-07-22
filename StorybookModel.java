42a15e4220987a169cc579bcb9b1f4b44bee823a
Shega Vala <81038951+Shega-V@users.noreply.github.com>
2021-07-21 22:05:40 -0400

    Add files via upload

---
+++ StorybookModel.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab9storybook;
+
+/**
+ *
+ * @author shegavala
+ */
+public class StorybookModel {
+    
+    private String userName;
+    private String crime;
+
+    
+    
+
+    public void setUserName(String _userName) {
+        userName = _userName;        
+    }
+
+    public String getCrime() {
+        return crime;
+    }
+
+    public void setCrime(String crime) {
+        this.crime = crime;
+    }
+
+    String getUsername() {
+        return userName;
+
+    }
+    
+    
+    
+    
+    
+}
